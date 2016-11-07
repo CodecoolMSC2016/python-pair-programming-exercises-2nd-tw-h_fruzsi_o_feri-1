25bec3421d76579a00527e88f3df324869e60867
FerencOnodi <ferenconodi91@gmail.com>
2016-11-07 20:08:33 +0100

    Solved palindrome

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,6 +1,11 @@
 def palindrome(str):
-    return
-
+    user_input = input("Enter a word: ")
+    u_input = user_input.replace(" ", "")
+    u_input = u_input.lower()
+    if u_input == u_input[::-1]:
+        return True
+    else:
+        return False
 
 def main():
     return
