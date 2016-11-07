f15fbda19bcc9f28089d5ceb75465b78d2228eea
FerencOnodi <ferenconodi91@gmail.com>
2016-11-07 21:33:51 +0100

    Password current version

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,33 @@
+import random, string
+
 def passwordgen():
-    return
+    length = 8
+    chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+    password = (''.join(random.choice(chars) for i in range(length)))
+    return password
 
 
 def main():
+    passwordgen()
     return
 
 
 if __name__ == '__main__':
     main()
+'''
+
+#length = 8
+#chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+#print(''.join(random.choice(chars) for i in range(length)))
+
+def passwordgen():
+    length = 8
+    letters = string.ascii_letters
+    numbers = string.digits
+    special = "!@#$%^&*()"
+    #chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+    chars = letters + numbers + special
+    password = (''.join(random.choice(chars) for i in range(length)))
+    print(password)
+    #return password
+passwordgen()'''
