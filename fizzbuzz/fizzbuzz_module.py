517fdd2f7e9b98aa63211a5f19d91b7162ba0254
Fuji91 <highwaytohell91@gmail.com>
2016-11-07 14:13:53 +0100

    ready

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
-    return
+    if number % 3 == 0 and number % 5 ==0:
+        return("FizzBuzz")
+    elif number % 3 == 0:
+        return ("Fizz")
+    elif number % 5 == 0:
+        return ("Buzz")    
+    else:
+        return(number)
+
 
 
 def main():
+    for number in range(1,101):
+        print(number)
     return
 
 if __name__ == '__main__':
